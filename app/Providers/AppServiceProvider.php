772c99cd8b2695829e64b2389f683cb5c3d8675a
Usman <razausmankhan97@gmail.com>
2024-04-14 16:11:04 +0500

    theme integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Page;
+use App\Observers\GenericObserver;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+            //partials._pages_menu
+            View::composer('partials._pages_menu', function ($view) {
+                $pages = Page::whereNull('parent_page_id')->with('sub_pages')->orderBy('sort_order', 'ASC')->get();
+                $view->with('pages', $pages);
+            });
+
+            // mode observers
+            // Product::observe(GenericObserver::class);
     }
 }
